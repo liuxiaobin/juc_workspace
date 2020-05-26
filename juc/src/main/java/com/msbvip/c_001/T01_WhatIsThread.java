f627e4ef0adae3928f1937dc43418d73fed8cad3
xbliu <305912675@qq.com>
2020-05-26 16:50:45 +0800

    新建项目

--- juc/src/main/java/com/msbvip/c_001/T01_WhatIsThread.java
+++ juc/src/main/java/com/msbvip/c_001/T01_WhatIsThread.java
@@ -1,5 +1,7 @@
 package com.msbvip.c_001;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Classname T01_WhatIsThread
  * @Description TODO
@@ -7,8 +9,34 @@ package com.msbvip.c_001;
  * @Created by xbliu
  */
 public class T01_WhatIsThread {
+    //程序是一个最小单元叫做线程，一个程序内不同的执行路径；
+    private static class T1 extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    TimeUnit.MICROSECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("T1");
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("asdasd");
+        //new T1().run();
+        new T1().start();
+        for (int j = 0; j < 10; j++) {
+            try {
+                TimeUnit.MICROSECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("main");
+        }
 
     }
 }
