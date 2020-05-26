64c86a9fecff93108bf45c0df2167a2f17af1d15
xbliu <305912675@qq.com>
2020-05-26 17:01:23 +0800

    新建项目

---
+++ juc/src/main/java/com/msbvip/c_001/T02_HowToCreatThread.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C), 2019-2020, 宇信融汇
+ * FileName: T01_HowToCreatThread
+ * Author:   xbliu
+ * Date:     2020/5/26 16:52
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * xbliu           修改时间           版本号              描述
+ */
+package com.msbvip.c_001;
+
+/**
+ * @Classname T01_HowToCreatThread
+ * @Description TODO
+ * @Date 2020/5/26 16:52
+ * @Created by xbliu
+ * @version
+ *  1、启动线程的三种方式 （Thread \Runnable\线程池）
+ */
+public class T02_HowToCreatThread {
+    static  class MyThread extends Thread{
+        @Override
+        public void run() {
+            System.out.println("Hello MyThread");
+        }
+    }
+    static  class Myrun implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Hello Myrun");
+        }
+    }
+
+    //lambda表达式启动线程
+    public static void main(String[] args) {
+        new MyThread().start();
+        new Thread(new Myrun()).start();
+        new Thread(()->{
+            System.out.println("Hello lambda");
+        }).start();
+    }
+
+}
