02e149cc73820f2b1a30310df916f780471dc02e
xbliu <305912675@qq.com>
2020-05-26 11:42:49 +0800

    新建项目

---
+++ juc/src/main/java/com/msbvip/c_001/T01_WhatIsThread.java
@@ -0,0 +1,14 @@
+package com.msbvip.c_001;
+
+/**
+ * @Classname T01_WhatIsThread
+ * @Description TODO
+ * @Date 2020/5/26 10:50
+ * @Created by xbliu
+ */
+public class T01_WhatIsThread {
+    public static void main(String[] args) {
+        System.out.println("asdasd");
+
+    }
+}
